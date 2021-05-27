be7c0e9b6e20d2e98f9eb235db8f5a9f9a0de081
cristian <cristiantcjtj@gmail.com>
2021-05-27 12:10:28 -0300

    Stored-Posts

--- Projects/my_site/blog/urls.py
+++ Projects/my_site/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page"),
     path("posts", views.AllPostsView.as_view(), name="post-page"),
-    # slug is acctualy a concept of route like = /posts/my-first-post where we can include symbols.
-    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>", views.SinglePostView.as_view(),
+          name="post-detail-page"), # slug is acctualy a concept of route like = /posts/my-first-post where we can include symbols.
+    path("read-later", views.ReadLaterView.as_view(), name="read-later" )
 ]
