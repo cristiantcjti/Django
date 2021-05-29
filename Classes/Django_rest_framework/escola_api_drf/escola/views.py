d079d5c1eed6402534f23ece6304751d594ac1c3
cristian <cristiantcjtj@gmail.com>
2021-05-29 19:55:22 -0300

    Django_Rest_Framework

---
+++ Classes/Django_rest_framework/escola_api_drf/escola/views.py
@@ -0,0 +1,50 @@
+from django.db.models.query import QuerySet
+from rest_framework import viewsets, generics
+from rest_framework import authentication
+from escola.models import Aluno, Curso, Matricula
+from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Class to show all students"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Class to show all courses"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Class to show all enrollments"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Class to list the student's enrollments"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Class to show all enrolled students"""    
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+
+    
