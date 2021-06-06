f48acbdd3393633eceb1c22f0f29a172fc619cbf
cristian <cristiantcjti@gmail.com>
2021-06-06 16:37:57 -0300

    Validating_Ordering

--- Projects/clientes-master/clientes/serializers.py
+++ Projects/clientes-master/clientes/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validator import *
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
     
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({"cpf":"Número de CPF inválido."})
+        
+
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({"nome":"Permitido apenas letras!"})
+
+
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({"rg":"Campo 'RG' deve conter 9 dígitos."})
+
+
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({"celular": "Campo 'Celular' deve seguir modelo: (11) 91234-5678."})
+                
+        return data
+
--- Projects/clientes-master/setup/settings.py
+++ Projects/clientes-master/setup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, os
 
+import django_heroku 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+django_heroku.settings(locals())
