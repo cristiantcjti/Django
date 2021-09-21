7a86780f879f481bc1f523cfc907417f4a1fee2e
cristian <cristiantcjti@gmail.com>
2021-09-21 19:57:30 -0300

    Permissions

--- Projects/drf-project/watchlist_app/api/views.py
+++ Projects/drf-project/watchlist_app/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from watchlist_app.models import WatchList, StreamPlatform, Review
 from watchlist_app.api.serializers import WatchSerializer, StreamPlatformSerializer, ReviewSerializer
 
@@ -31,6 +32,7 @@ class ReviewCreate(generics.CreateAPIView):
 class ReviewList(generics.ListAPIView):
     #queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         pk = self.kwargs['pk']
@@ -38,7 +40,8 @@ class ReviewList(generics.ListAPIView):
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
-    serializer_class = ReviewSerializer    
+    serializer_class = ReviewSerializer  
+    permission_classes = [IsAuthenticatedOrReadOnly]  
 
 
 ######################
